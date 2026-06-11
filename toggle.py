508aabc336f63361cb040fab1ef48f44ecc746d0
Mikhail Velichko <efklid@gmail.com>
2026-06-11 13:06:59 +0300

    reliability and security hardening

--- toggle.py
+++ toggle.py
@@ -20,7 +20,6 @@ def load_conf(path='/etc/touchpad-toggle.conf'):
 
 
 def get_active_session():
-    """Returns (uid, desktop) from logind on the system bus."""
     system_bus = dbus.SystemBus()
     seat_props = dbus.Interface(
         system_bus.get_object('org.freedesktop.login1', '/org/freedesktop/login1/seat/seat0'),
@@ -32,8 +31,11 @@ def get_active_session():
         'org.freedesktop.DBus.Properties',
     )
     uid, _ = session_props.Get('org.freedesktop.login1.Session', 'User')
+    uid = int(uid)
+    if uid == 0:
+        raise RuntimeError('active session belongs to root; no user session bus available')
     desktop = str(session_props.Get('org.freedesktop.login1.Session', 'Desktop'))
-    return int(uid), desktop
+    return uid, desktop
 
 
 def session_bus(uid):
@@ -67,28 +69,33 @@ def toggle_non_kde(bus):
     icon  = 'input-touchpad-symbolic'  if inhibited else 'touchpad-disabled-symbolic'
     label = 'Touchpad enabled'         if inhibited else 'Touchpad disabled'
 
-    notif = bus.get_object('org.freedesktop.Notifications', '/org/freedesktop/Notifications')
-    dbus.Interface(notif, 'org.freedesktop.Notifications').Notify(
-        'touchpad', dbus.UInt32(0), icon, 'Touchpad', label,
-        dbus.Array([], signature='s'),
-        dbus.Dictionary({'urgency': dbus.Byte(1)}, signature='sv'),
-        dbus.Int32(2000),
-    )
+    try:
+        notif = bus.get_object('org.freedesktop.Notifications', '/org/freedesktop/Notifications')
+        dbus.Interface(notif, 'org.freedesktop.Notifications').Notify(
+            'touchpad', dbus.UInt32(0), icon, 'Touchpad', label,
+            dbus.Array([], signature='s'),
+            dbus.Dictionary({'urgency': dbus.Byte(1)}, signature='sv'),
+            dbus.Int32(2000),
+            timeout=2000,
+        )
+    except dbus.DBusException as e:
+        print(f'touchpad-toggle: notification failed: {e}', file=sys.stderr)
 
 
 def toggle_kde(bus):
     accel = bus.get_object('org.kde.kglobalaccel', '/component/org_kde_touchpadshortcuts_desktop')
-    dbus.Interface(accel, 'org.kde.kglobalaccel.Component').invokeShortcut('ToggleTouchpad')
+    dbus.Interface(accel, 'org.kde.kglobalaccel.Component').invokeShortcut(
+        'ToggleTouchpad', timeout=2000,
+    )
 
 
 def do_toggle():
     lock = open('/var/lock/touchpad-toggle.lock', 'w')
     try:
-        fcntl.flock(lock, fcntl.LOCK_EX | fcntl.LOCK_NB)
-    except BlockingIOError:
-        lock.close()
-        return
-    try:
+        try:
+            fcntl.flock(lock, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except BlockingIOError:
+            return
         uid, desktop = get_active_session()
         bus = session_bus(uid)
         if desktop.lower() in ('kde', 'plasma'):
